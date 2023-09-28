e9a8ace5adceaf70cdd0bb1768663454776e545e
Dudrine Richemond <dudrinex@gmail.com>
2023-09-28 23:49:00 +0000

    #32 enabled static website hosting, uploaded index and error file via terraform

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -21,3 +21,43 @@ resource "aws_s3_bucket" "website_bucket" {
    UserUUID  = var.user_uuid
  }
 }
+
+#resource "aws_s3_bucket" "static_website" {
+#  bucket = aws_s3_bucket.website_bucket.bucket
+#  #acl    = "public-read"
+#
+#  website {
+#    index_document = "index.html"  # Set your main HTML file
+#    error_document = "error.html"  # Set your error HTML file (optional)
+#  }
+#}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "index.html"
+  source = var.index_html_filepath
+
+  etag = filemd5(var.index_html_filepath)
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "error.html"
+  source = var.error_html_filepath
+
+  etag = filemd5(var.error_html_filepath)
+}
