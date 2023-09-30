19cbd4f0c5b6f91a41caf462faec3e0a1e8d642f
Dudrine Richemond <dudrinex@gmail.com>
2023-09-30 00:09:16 +0000

    #34 create cdn and served s3 bucket from cdn

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -7,57 +7,6 @@ terraform {
   }
 }
 
-#provider "aws" {
-#  # Configuration options
-#}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity
+data "aws_caller_identity" "current" {}
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-  # Bucket Naming Rules
-  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
-  bucket = var.bucket_name
-
- tags = {
-   UserUUID  = var.user_uuid
- }
-}
-
-#resource "aws_s3_bucket" "static_website" {
-#  bucket = aws_s3_bucket.website_bucket.bucket
-#  #acl    = "public-read"
-#
-#  website {
-#    index_document = "index.html"  # Set your main HTML file
-#    error_document = "error.html"  # Set your error HTML file (optional)
-#  }
-#}
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
-resource "aws_s3_bucket_website_configuration" "website_configuration" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
-resource "aws_s3_object" "index_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "index.html"
-  source = var.index_html_filepath
-
-  etag = filemd5(var.index_html_filepath)
-}
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
-resource "aws_s3_object" "error_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "error.html"
-  source = var.error_html_filepath
-
-  etag = filemd5(var.error_html_filepath)
-}
