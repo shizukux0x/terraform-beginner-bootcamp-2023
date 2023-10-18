06446d565b211cc37f5c4ab759a92a025ed9e4dc
Dudrine Richemond <dudrinex@gmail.com>
2023-10-18 01:47:16 +0000

    #43 finish implementation of terraform provider

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
   
@@ -38,3 +38,14 @@ provider "terratowns" {
 #  content_version = var.content_version
 #  assets_path = var.assets_path
 #}
+
+resource "terratowns_home" "home" {
+  name = "My Top Songs of 2023!"
+  description = <<DESCRIPTION
+  This is a list my favorite 15 songs of 2023, with short descriptions and links to each.
+  DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "2n8g1kls.cloudfront.net"
+  town = "video-valley"
+  content_version = 1.0
+}
